74d149ba0ff50e73f7031e2aca2a36beaeb0efba
Guneet kaur <guneet.kaur.15cse@bml.edu.in>
2018-08-21 01:15:03 +0530

    Co-Relation between fixed acidity and quality of wine
    It can be clearly evident from running the script that fixed acidity has a weak relation with correlation value r= 0.12.  This feature independently is not sufficient to define the quality of the wine.

---
+++ fixedacidityvsquality.py
@@ -0,0 +1,58 @@
+import pandas as pd
+import numpy as np
+from cmath import sqrt
+import scipy.stats as stats
+from matplotlib import pyplot as plt
+from sklearn.linear_model import LinearRegression
+
+data = pd.read_csv('winequality-red.csv')
+
+X = data['fixed acidity'].values[:,np.newaxis]
+y = data['quality'].values
+
+xtotal=0
+ytotal =0
+xytotal =0
+x2total =0
+y2total =0
+n =1599
+
+for i in range(n):
+    xtotal +=X[i]
+
+for i in range(n):
+    ytotal +=y[i]
+
+for i in range(n):
+    xytotal +=(X[i]*y[i])
+    
+for i in range(n):
+    x2total +=(X[i]*X[i])
+    
+for i in range(n):
+    y2total +=(y[i]*y[i])
+
+
+num = xytotal - ((xtotal*ytotal)/n)
+
+den1 = (x2total - ((xtotal*xtotal)/n))
+
+den2 = (y2total - ((ytotal*ytotal)/n))
+
+den = den1*den2
+denf = sqrt(den)
+r = num/denf
+print("Correlation for age and charges")
+print(r)
+
+model = LinearRegression()
+
+model.fit(X, y)
+plt.scatter(X, y,color='blue')
+plt.plot(X, model.predict(X),color='k')
+plt.title('quality Vs fixed acid ', fontsize=14)
+plt.xlabel('fixed acid', fontsize=14)
+plt.ylabel('quality', fontsize=14)
+plt.show()
+print('Intercept: \n', model.intercept_)
+print('Coefficients: \n', model.coef_)
