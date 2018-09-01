05e92d7622d7f3c35dabbe6134af3fb899b74308
Guneet kaur <guneet.kaur.15cse@bml.edu.in>
2018-09-01 16:42:49 +0530

    used train_test_split function
    Increased the variance score to 28% from 25%.

--- Multivarible Regression/Multi_Regression.py
+++ Multivarible Regression/Multi_Regression.py
@@ -2,20 +2,20 @@ import pandas as pd
 from pandas import DataFrame
 from matplotlib import pyplot as plt
 from sklearn import linear_model
-import statsmodels.api as sm
-from mpl_toolkits import mplot3d
 from sklearn.metrics import mean_squared_error, r2_score
+from sklearn.model_selection import train_test_split
 
 data = pd.read_csv('winequality-red.csv')
 df = DataFrame(data,columns=['pH','density','alcohol','quality'])
 
 X = df[['pH','density','alcohol']].astype(float) # here we have 2 variables for multiple regression. If you just want to use one variable for simple linear regression, then use X = df['Interest_Rate'] for example.Alternatively, you may add additional variables within the brackets
-Y = df['quality'].astype(float)
+y = df['quality'].astype(float)
 
+X_train,X_test,y_train,y_test = train_test_split(X,y,random_state=0)
 regr = linear_model.LinearRegression()
-regr.fit(X, Y)
+regr.fit(X_train, y_train)
 print('Intercept: \n', regr.intercept_)
 print('Coefficients: \n', regr.coef_)
 
-a = regr.predict(X)
-print('Variance score: %.2f' % r2_score(z, a))
+y_pred = regr.predict(X_test)
+print('Variance score: %.2f' % r2_score(y_test, y_pred))
